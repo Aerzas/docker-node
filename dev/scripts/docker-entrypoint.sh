fc0a1577f165842dffca6d371bc1cc4df3d78972
Aerzas <nicolas.ricklin@protonmail.com>
2026-06-04 22:59:13 +0200

    Fix NSS group

--- dev/scripts/docker-entrypoint.sh
+++ dev/scripts/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Mock group and passwd files
-echo "${USER_GROUP}:x:$(id -g)" >> "${NSS_WRAPPER_GROUP}";
+echo "${USER_GROUP}:x:$(id -g):" >> "${NSS_WRAPPER_GROUP}";
 echo "${USER_NAME}:x:$(id -u):$(id -g):${USER_NAME}:${USER_HOME}:/sbin/nologin" >> "${NSS_WRAPPER_PASSWD}";
 
 exec "$@"
